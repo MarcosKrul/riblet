a702d1f8b284e2c00afd609af639ed054f02a8ec
Marcos Krul <marcoskrul2k@gmail.com>
2021-12-06 20:08:19 -0300

    utils

---
+++ myutils/CoordinateSystem.java
@@ -0,0 +1,43 @@
+package myutils;
+
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+
+public abstract class CoordinateSystem {
+
+  public final static int X_OFFSET = 50;
+  public final static int Y_OFFSET = 50;
+
+  public static void draw(Graphics2D g2d, TwoAxisCoordinate max, int step) {
+    String s;
+    
+    // Remember the actual font.
+    Font fo = g2d.getFont();
+    
+    // Use a small font.
+    g2d.setFont(new Font("sansserif", Font.PLAIN, 9));
+    
+    // x-axis.
+    g2d.drawLine(X_OFFSET, Y_OFFSET, max.x, Y_OFFSET);
+    // Marks and labels for the x-axis.
+    for (int i = X_OFFSET + step; i <= max.x; i = i + step) {
+      g2d.drawLine(i, Y_OFFSET - 2, i, Y_OFFSET + 2);
+      g2d.drawString(String.valueOf(i), i - 7, Y_OFFSET - 7);
+    }
+
+    // y-axis.
+    g2d.drawLine(X_OFFSET, Y_OFFSET, X_OFFSET, max.y);
+    // Marks and labels for the y-axis.
+    s = "  "; // for indention of numbers < 100
+    for (int i = Y_OFFSET + step; i <= max.y; i = i + step) {
+      g2d.drawLine(X_OFFSET - 2, i, X_OFFSET + 2, i);
+      if (i > 99) s = "";
+      g2d.drawString(s + String.valueOf(i), X_OFFSET - 25, i + 5);
+    }
+
+    // Reset to the original font.
+    g2d.setFont(fo);
+  }
+
+}
---
+++ myutils/TwoAxisCoordinate.java
@@ -0,0 +1,11 @@
+package myutils;
+
+public class TwoAxisCoordinate {
+  public int x;
+  public int y;
+
+  public TwoAxisCoordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
