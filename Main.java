e5023371e5902214f57b20d682a12bdcebbb3487
Marcos Krul <marcoskrul2k@gmail.com>
2021-12-07 15:57:19 -0300

    main

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] argv) {
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      System.out.println("Insira o tamanho da amostra: ");
+      int sampleLength = scanner.nextInt();
+
+      System.out.println("Insira a altura de cada riblet: ");
+      double height = scanner.nextDouble();
+      
+      System.out.println("Insira o ângulo: ");
+      double angle = scanner.nextDouble();
+      
+      System.out.println("Insira a distância entre os centros: ");
+      double distanceBetween = scanner.nextDouble();
+      
+      System.out.println("Insira a distância do início ao primeiro centro: ");
+      double distanceFirst = scanner.nextDouble();
+
+      try {
+        Riblet riblet = new Riblet("Estrutura Riblet", sampleLength, height, angle, distanceBetween, distanceFirst);
+        riblet.setVisible(true);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      
+    }
+  }
+
+}
